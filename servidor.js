74e32c99126f9a106db020e70e6f8fa5743bb6f3
PHGodoyCosta <69993249+PHGodoyCosta@users.noreply.github.com>
2021-09-15 15:00:05 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,22 +1,22 @@
 const http = require("http")
 const events = require("events")
-const fs = require("fs")
+const express = require("express")
 
+const app = express()
+const servidor = http.createServer(app)
 const host = "127.0.0.1"
 const eventos = new events.EventEmitter()
 eventos.on("log", () => {console.log("Macaco entrou no sevidor")})
 eventos.on("out", () => {console.log("Macaco saiu do servidor")})
 
-const servidor = http.createServer((request, response) => {
-    if (request.url == "/") {
-        fs.readFile("index.html", (err, file) => {
-            if (err) throw err;
-            eventos.emit("log")
-            response.writeHead(200, {"Content-type":"text/html"})
-            response.end(file)
-            eventos.emit("out")
-        })
-    }
+app.set("view engine", "html")
+app.use(express.static(__dirname))
+
+app.get("/", (request, response) => {
+    response.status(200)
+    eventos.emit("log")
+    response.sendFile(__dirname + "/index.html")
+    eventos.emit("out")
 })
 
 servidor.listen(process.env.PORT || 80, host, () => {
